ec5821793a979492d2a43770ef6b3784698c3dee
Eliot Luna <lunel288@student.otago.ac.nz>
2022-08-08 05:32:19 +1200

    Basic Translation Program. Translate button + Input Textfield + Output Textfield. Next step is adding camera input and image recognition.

--- app/src/main/java/com/example/imagelingo/MainActivity.kt
+++ app/src/main/java/com/example/imagelingo/MainActivity.kt
@@ -2,10 +2,53 @@ package com.example.imagelingo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val wordList = listOf<String>("Pen", "Laptop", "Clock")
+    private val translationList = listOf<String>("Pene", "Rorohiko", "Karaka")
+    private lateinit var btnTranslate: Button
+    private lateinit var txtInput: TextView
+    private lateinit var txtOutput: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTranslate = findViewById(R.id.btnTranslate)
+        txtInput = findViewById(R.id.txtInput)
+        txtOutput = findViewById(R.id.txtOutput)
+
+        btnTranslate.setOnClickListener(this)
+
+
+        }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnTranslate -> {
+                val input: String = txtInput.text.toString()
+                for ( i  in 0..(translationList.size-1)) {
+                    if (input == wordList[i]) {
+                        txtOutput.setText(translationList[i])
+                        break
+                    }
+                    else{ //word not in list
+                        txtOutput.setText(R.string.invalid_word)
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    fun main(){
+
+
     }
 }
