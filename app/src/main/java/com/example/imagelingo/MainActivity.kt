a675a2f1245baa3f735d142c66fcae2301106d7c
Eliot Luna <lunel288@student.otago.ac.nz>
2022-08-27 12:16:52 +1200

    Added camera input. Next step is adding image recognition.

--- app/src/main/java/com/example/imagelingo/MainActivity.kt
+++ app/src/main/java/com/example/imagelingo/MainActivity.kt
@@ -1,32 +1,52 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.imagelingo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val wordList = listOf<String>("Pen", "Laptop", "Clock")
     private val translationList = listOf<String>("Pene", "Rorohiko", "Karaka")
     private lateinit var btnTranslate: Button
+    private lateinit var btnCapture: Button
     private lateinit var txtInput: TextView
     private lateinit var txtOutput: TextView
-
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCapture = findViewById(R.id.btnCapture)
         btnTranslate = findViewById(R.id.btnTranslate)
         txtInput = findViewById(R.id.txtInput)
         txtOutput = findViewById(R.id.txtOutput)
+        imageView = findViewById(R.id.imageView)
 
         btnTranslate.setOnClickListener(this)
+        btnCapture.setOnClickListener(this)
 
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val takenImage = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(takenImage)
         }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 
     override fun onClick(v: View?) {
         when (v?.id) {
@@ -42,13 +62,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     }
                 }
             }
+
+            R.id.btnCapture -> {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                if(takePictureIntent.resolveActivity(this.packageManager) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                } else{
+                    Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
-
 
 
     fun main(){
 
 
+    }
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 42
     }
 }
