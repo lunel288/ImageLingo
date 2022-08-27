13160e6c3032e47def4e95d1d104e4c91e5eef36
Eliot Luna <lunel288@student.otago.ac.nz>
2022-08-27 12:47:50 +1200

    Added image recognition.

--- app/src/main/java/com/example/imagelingo/MainActivity.kt
+++ app/src/main/java/com/example/imagelingo/MainActivity.kt
@@ -4,7 +4,7 @@ package com.example.imagelingo
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
+import android.graphics.*
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -13,8 +13,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+
+        private const val REQUEST_IMAGE_CAPTURE = 42
+        private const val MAX_FONT_SIZE = 96F
+    }
 
     private val wordList = listOf<String>("Pen", "Laptop", "Clock")
     private val translationList = listOf<String>("Pene", "Rorohiko", "Karaka")
@@ -42,7 +53,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             val takenImage = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(takenImage)
+            //imageView.setImageBitmap(takenImage)
+            setViewAndDetect(takenImage)
+
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
@@ -75,13 +88,119 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * runObjectDetection(bitmap: Bitmap)
+     *      TFLite Object Detection function
+     */
+    private fun runObjectDetection(bitmap: Bitmap) {
+
+        // Step 1: create TFLite's TensorImage object
+        val image = TensorImage.fromBitmap(bitmap)
+
+        // Step 2: Initialize the detector object
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+            .setMaxResults(5)
+            .setScoreThreshold(0.5f)
+            .build()
+        val detector = ObjectDetector.createFromFileAndOptions(
+            this, // the application context
+            "model.tflite", // must be same as the filename in assets folder
+            options
+        )
+
+        // Step 3: feed given image to the model and print the detection result
+        val results = detector.detect(image)
+
+        // Step 4: Parse the detection result and show it
+        val resultToDisplay = results.map {
+            // Get the top-1 category and craft the display text
+            val category = it.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            // Create a data object to display the detection result
+            DetectionResult(it.boundingBox, text)
+        }
+// Draw the detection result on the bitmap and show it.
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            imageView.setImageBitmap(imgWithResult)
+        }
+
+    }
+
+    /**
+     * drawDetectionResult(bitmap: Bitmap, detectionResults: List<DetectionResult>
+     *      Draw a box around each objects and show the object's name.
+     */
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = MAX_FONT_SIZE
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
+    }
+
+
+    /**
+    * setViewAndDetect(bitmap: Bitmap)
+    *      Set image to view and call object detection
+    */
+    private fun setViewAndDetect(bitmap: Bitmap) {
+        // Display capture image
+        imageView.setImageBitmap(bitmap)
+
+
+        // Run ODT and display result
+        // Note that we run this in the background thread to avoid blocking the app UI because
+        // TFLite object detection is a synchronised process.
+        lifecycleScope.launch(Dispatchers.Default) { runObjectDetection(bitmap) }
+    }
+
+
+/**
+ * DetectionResult
+ *      A class to store the visualization info of a detected object.
+ */
+data class DetectionResult(val boundingBox: RectF, val text: String)
 
     fun main(){
 
 
     }
 
-    companion object {
-        private const val REQUEST_IMAGE_CAPTURE = 42
-    }
+
 }
